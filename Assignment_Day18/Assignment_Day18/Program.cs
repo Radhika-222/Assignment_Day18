26edfd9d06c3e90f7e22539e9cce31eada2bfa54
Radhika-222 <wandareradhika@gmail.com>
2023-11-29 16:57:27 +0530

    Assignment

---
+++ Assignment_Day18/Assignment_Day18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Day18
+{
+    internal delegate void GameProbability();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            GameProbability GamePro = new GameProbability(game.Players);
+            GamePro();
+            Console.ReadKey();
+        }
+    }
+}
